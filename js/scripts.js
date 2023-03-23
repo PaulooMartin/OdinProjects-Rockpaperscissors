37bcc1c313f019211a2555f4bd757298ca222a64
Paul Martin <paulmartinblopez@gmail.com>
2023-03-23 16:50:47 +0800

    Added multiple functions and scorecount
    Added function for running the entire game with desired rounds.
    Added function for determining game's overall winner.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+const gameRounds = 5;
+let playerScore = 0;
+let computerScore = 0;
+
+game(gameRounds);
+
 // Each player will give their choice,
 //    For the computer: randomly chosen
 function getComputerChoice(){
@@ -52,30 +58,72 @@ function gameOn(playerSelection, computerSelection){
             case playerSelection === "rock":
                 if (computerSelection === "paper"){
                     roundResult = "You Lose! Paper beats Rock";
+                    computerScore++
                 } else {
                     roundResult = "You Win! Rock beats Scissors"
+                    playerScore++
                 }
                 break;
 
             case playerSelection === "scissors":
                 if (computerSelection === "rock"){
                     roundResult = "You Lose! Rock beats Scissors";
+                    computerScore++
                 } else {
-                    roundResult = "You Win! Scissors beats Paper"
+                    roundResult = "You Win! Scissors beats Paper";
+                    playerScore++
                 }
                 break;
 
             case playerSelection === "paper":
                 if (computerSelection === "scissors"){
                     roundResult = "You Lose! Scissors beats Paper";
+                    computerScore++
                 } else {
                     roundResult = "You Win! Paper beats Rock"
+                    playerScore++
                 }
                 break;
 
             default:
-                roundResult = "Something somewhere is wrong."
+                roundResult = "Something somewhere is wrong. Must have been your answer T_T"
     }
 }
     return roundResult;
+}
+
+function calcChampion(){
+    switch (true){
+        case (playerScore === computerScore):
+            console.log("Nobody won the game");
+            break;
+
+        case (playerScore > computerScore):
+            console.log("%cYou won! Beat that, piece of machine!", "color: green; font-weight: bold;");
+            break;
+
+        case (computerScore > playerScore):
+            console.log("%cYou lost. Nobody saw that right?", "color: red; font-weight: bold;");
+            break;
+
+        default:
+            alert("Something somewhere went wrong. Function: calcChampion");
+    }
+    playerScore = 0;
+    computerScore = 0;
+    return;
+}
+
+// The game will have x number of rounds.
+function game(roundCount){
+    for (i = 1; i <= roundCount; i++){
+        console.log (`%cRound ${i}`, "color: darkblue; font-weight: bold;");
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerChoice();
+        let finalResult  = gameOn(playerSelection, computerSelection);
+        console.log(finalResult);
+    }
+    calcChampion();
+    console.log ("%c-----Game End-----", "color: darkblue; font-weight: bolder");
+    return;
 }
