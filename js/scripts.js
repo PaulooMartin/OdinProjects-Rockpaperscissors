4cf7aa589af6e9d37793c58ea93ff84bbc06b729
Paul Martin <paulmartinblopez@gmail.com>
2023-03-23 14:17:15 +0800

    Added function for determining results

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ function getComputerChoice(){
     let computerChoice;
     switch(true){
         case randomChoice === 0:
-            computerChoice = "Rock";
+            computerChoice = "rock";
             break;
 
         case randomChoice === 1:
-            computerChoice = "Paper";
+            computerChoice = "paper";
             break;
 
         case randomChoice === 2:
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
             break;
     }
     return computerChoice;
@@ -22,8 +22,14 @@ function getComputerChoice(){
 function getPlayerChoice(){
 // Prompt user
     let userChoice = prompt("Rock, paper, or scissors?");
+    if (userChoice === null){
+        alert("You did not choose anything!");
+        return;
+    }
+
     userChoice = userChoice.toLowerCase();
-// Check validity of choice
+
+    // Check validity of choice
     switch (true){
         case userChoice === "rock":
         case userChoice === "paper":
@@ -31,7 +37,45 @@ function getPlayerChoice(){
             return userChoice;
 
         default:
-            alert("You did not choose anything!");
+            alert("You typed it wrong :(");
             return;
     }
+}
+
+// After each choice has been taken, compare their choices and determine the winner
+function gameOn(playerSelection, computerSelection){
+    let roundResult;
+    if (playerSelection === computerSelection){
+        roundResult = "This round is tied!"
+    } else{
+        switch (true){
+            case playerSelection === "rock":
+                if (computerSelection === "paper"){
+                    roundResult = "You Lose! Paper beats Rock";
+                } else {
+                    roundResult = "You Win! Rock beats Scissors"
+                }
+                break;
+
+            case playerSelection === "scissors":
+                if (computerSelection === "rock"){
+                    roundResult = "You Lose! Rock beats Scissors";
+                } else {
+                    roundResult = "You Win! Scissors beats Paper"
+                }
+                break;
+
+            case playerSelection === "paper":
+                if (computerSelection === "scissors"){
+                    roundResult = "You Lose! Scissors beats Paper";
+                } else {
+                    roundResult = "You Win! Paper beats Rock"
+                }
+                break;
+
+            default:
+                roundResult = "Something somewhere is wrong."
+    }
+}
+    return roundResult;
 }
